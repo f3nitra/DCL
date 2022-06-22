4204991c82d7f3ddd7a6ddc49684c550a21ce67e
Andriamaromanana Fenitra <68927288+f3nitra@users.noreply.github.com>
2022-06-22 22:10:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("menu-nav").style.top = "0";
+    } else {
+        document.getElementById("menu-nav").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+};
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+};
